98ada15164992a74cd0f3249a230a6fb953f3c12
Scotty P <provencher.scottucf@gmail.com>
2020-11-05 02:00:21 -0500

    creating tables 2

--- employee_schema.sql
+++ employee_schema.sql
@@ -1,3 +1,4 @@
+--DROP Table departments
 CREATE TABLE departments (
 	dept_no VARCHAR(10) PRIMARY KEY,
 	dept_name VARCHAR(30)
@@ -5,13 +6,8 @@ CREATE TABLE departments (
 
 select * FROM departments
 
-CREATE TABLE dept_employees (
-	emp_no INT PRIMARY KEY,
-	dept_no VARCHAR (10),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
+--DROP Table employees
 
-select * FROM dept_employees
 	
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY,
@@ -24,5 +20,31 @@ CREATE TABLE employees (
 );
 
 select * FROM employees
+
+--DROP Table dept_employees
+CREATE TABLE dept_employees (
+	emp_no INT PRIMARY KEY,
+	dept_no VARCHAR (10),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+select * FROM dept_employees
+
+--DROP Table salaries
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+select * FROM salaries
+
+
+
+
+
+
+
 
 
