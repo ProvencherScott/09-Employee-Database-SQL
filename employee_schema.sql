741bd4e4c00c1b421434e626211f4aaea2d20579
Scotty P <provencher.scottucf@gmail.com>
2020-11-06 07:44:51 -0500

    import data

--- employee_schema.sql
+++ employee_schema.sql
@@ -1,4 +1,12 @@
-DROP Table departments
+--Drop TABLE titles
+CREATE TABLE titles (
+	title_id VARCHAR(5) NOT NULL PRIMARY KEY,
+	title VARCHAR (30) NOT NULL
+);
+
+Select * FROM titles
+
+--DROP Table departments
 
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
@@ -8,9 +16,8 @@ CREATE TABLE departments (
 select * FROM departments
 
 
-DROP Table employees
+--DROP Table employees
 
-	
 CREATE TABLE employees (
 	emp_no INT NOT NULL PRIMARY KEY,
 	emp_title_id VARCHAR (5) NOT NULL,
@@ -18,39 +25,46 @@ CREATE TABLE employees (
 	first_name VARCHAR (30) NOT NULL,
 	last_name VARCHAR (30) NOT NULL,
 	sex VARCHAR (10) NOT NULL,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
+		FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 select * FROM employees
 
-DROP Table dept_employees
+--DROP Table dept_employees
 
 CREATE TABLE dept_employees (
-	emp_no INT PRIMARY KEY,
-	dept_no VARCHAR (10),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT NOT NULL,
+	dept_no VARCHAR (4) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+		FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	
+	--FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	--FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 select * FROM dept_employees
 
-DROP Table salaries
+--DROP Table salaries
 
 CREATE TABLE salaries (
 	emp_no INT NOT NULL PRIMARY KEY,
-	salary INT,
+	salary INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 select * FROM salaries
 
-Drop TABLE titles
-CREATE TABLE titles (
-	title_id VARCHAR(5) NOT NULL PRIMARY KEY
-	title VARCHAR (30),
-	
+--DROP TABLE dept_manager
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR (4) NOT NULL,
+	emp_no INT NOT NULL PRIMARY KEY,
+		FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+		FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+Select * FROM dept_manager
 
 
 
