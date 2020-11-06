c8d764631351e223147238cb95847a3b084deaee
Scotty P <provencher.scottucf@gmail.com>
2020-11-05 20:38:33 -0500

    editing

--- employee_schema.sql
+++ employee_schema.sql
@@ -1,27 +1,30 @@
---DROP Table departments
+DROP Table departments
+
 CREATE TABLE departments (
-	dept_no VARCHAR(10) PRIMARY KEY,
-	dept_name VARCHAR(30)
+	dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
 );
 
 select * FROM departments
 
---DROP Table employees
+
+DROP Table employees
 
 	
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR (10),
-	birth_date DATE,
-	first_name VARCHAR (30),
-	last_name VARCHAR (30),
-	sex VARCHAR (10),
-	hire_date DATE
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR (5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR (30) NOT NULL,
+	last_name VARCHAR (30) NOT NULL,
+	sex VARCHAR (10) NOT NULL,
+	hire_date DATE NOT NULL
 );
 
 select * FROM employees
 
---DROP Table dept_employees
+DROP Table dept_employees
+
 CREATE TABLE dept_employees (
 	emp_no INT PRIMARY KEY,
 	dept_no VARCHAR (10),
@@ -31,14 +34,22 @@ CREATE TABLE dept_employees (
 
 select * FROM dept_employees
 
---DROP Table salaries
+DROP Table salaries
+
 CREATE TABLE salaries (
-	emp_no INT PRIMARY KEY,
+	emp_no INT NOT NULL PRIMARY KEY,
 	salary INT,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 select * FROM salaries
+
+Drop TABLE titles
+CREATE TABLE titles (
+	title_id VARCHAR(5) NOT NULL PRIMARY KEY
+	title VARCHAR (30),
+	
+);
 
 
 
