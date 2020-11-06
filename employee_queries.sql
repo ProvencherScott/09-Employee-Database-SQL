d760af078934d5810b4d33066edc4ad7c07b9173
Scotty P <provencher.scottucf@gmail.com>
2020-11-06 18:52:12 -0500

    change

--- employee_queries.sql
+++ employee_queries.sql
@@ -1,4 +1,4 @@
---List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT e.emp_no, 
 e.last_name,
 e.first_name, 
@@ -7,7 +7,7 @@ s.salary
 FROM employees as e
 JOIN salaries s ON (e.emp_no = s.emp_no);
 
-
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
 
 
