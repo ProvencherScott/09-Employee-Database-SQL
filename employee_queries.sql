046c9c2db769fb4a533d47c532e985d7dfa77985
Scotty P <provencher.scottucf@gmail.com>
2020-11-07 02:13:50 -0500

    query #7

--- employee_queries.sql
+++ employee_queries.sql
@@ -59,4 +59,12 @@ WHERE d.dept_name = 'Sales'
 -- 7. List all employees in the Sales and Development departments, including their employee number, 
 -- last name, first name, and department name.
 
+SELECT e.emp_no "Employee Number", e.last_name "Last Name",
+e.first_name "First Name", d.dept_name "Department Name"
+FROM employees e
+JOIN dept_emp de ON (e.emp_no = de.emp_no)
+JOIN departments d ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+
+
 
